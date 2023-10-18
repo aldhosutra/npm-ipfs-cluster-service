2d3eb4b916112c8fe991b64dcda70e94bdff769d
Aldo Suhartono Putra <aldhosutra@gmail.com>
2023-10-18 21:37:48 +0800

    update tests and readme

--- test/path.js
+++ test/path.js
@@ -5,15 +5,11 @@ var cp = require("child_process");
 var ipfs = path.join(__dirname, "..", "bin", "ipfs-cluster-service");
 
 test("ensure ipfs-cluster-service bin path exists", function (t) {
-  t.plan(4);
+  t.plan(3);
   fs.stat(ipfs, function (err, stats) {
     t.error(err, "ipfs-cluster-service bin should stat witout error");
     cp.exec([ipfs, "version"].join(" "), function (err, stdout, stderr) {
       t.error(err, "ipfs-cluster-service runs without error");
-      t.true(
-        stdout.indexOf("ipfs-cluster-service version") >= 0,
-        "ipfs-cluster-service version retreived"
-      );
       t.false(stderr, "no stderr output");
     });
   });
